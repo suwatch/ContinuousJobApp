10c526173b58398d46565757930379abfe303243
Suwath Ch <suwatch@microsoft.com>
2016-10-27 11:28:52 -0700

    initial commit

---
+++ ContinuousJobApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace ContinuousJobApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = 10;
+            Console.WriteLine("Begin sleep {0}s ..", sec);
+            Thread.Sleep(sec * 1000);
+            Console.WriteLine("End sleep {0}s ..", sec);
+        }
+    }
+}
