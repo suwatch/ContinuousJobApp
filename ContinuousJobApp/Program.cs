1b81eec15069c35a355c4a3e12193d635078ed86
Suwath Ch <suwatch@microsoft.com>
2016-10-27 13:31:01 -0700

    add shutdown support

--- ContinuousJobApp/Program.cs
+++ ContinuousJobApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace ContinuousJobApp
@@ -7,12 +8,21 @@ namespace ContinuousJobApp
     {
         static void Main(string[] args)
         {
+            var shutdownFile = Environment.GetEnvironmentVariable("WEBJOBS_SHUTDOWN_FILE");
             int loop = 10;
             Console.WriteLine("Begin Run");
             while (--loop > 0)
             {
                 Console.WriteLine("Loop {0}", loop);
-                Thread.Sleep(30000);
+                for (int i = 0; i < 6; ++i)
+                {
+                    Thread.Sleep(5000);
+                    if (File.Exists(shutdownFile))
+                    {
+                        Console.WriteLine("Shutdown file exists {0}", shutdownFile);
+                        break;
+                    }
+                }
             }
             Console.WriteLine("End Run");
         }
